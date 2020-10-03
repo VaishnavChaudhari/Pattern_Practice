88a73b47afe79a9baceb1f5fac55a05b6e967ec7
VaishnavChaudhari <65059084+VaishnavChaudhari@users.noreply.github.com>
2020-10-03 11:25:27 +0530

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,25 +8,80 @@ public class Main {
 
 
         // Patters Tutorial
-        // * * * * * * *
-        //* * * * * *
-        //* * * * *
-        //* * * *
-        //* * *
-        //* *
-        //*
+//        * * * * *
+//          * * * *
+//            * * *
+//              * *
+//                *
 
         Scanner cs = new Scanner(System.in);
-        int userValue = cs.nextInt();
+                int n = cs.nextInt();
 
-        for (int i = 0; i <= userValue; i++) {
-            for (int j = 0; j < userValue-i; j++) {
+//                for (int i = 0; i <= n  ; i++) {
+//                    for (int j = 0; j < i - 1 + 1 ; j++) {
+//                        System.out.print("  ");
+//                    }
+//                    for (int j = 0; j < n -i  ; j++) {
+//                        System.out.print("* ");
+//                    }
+//                    System.out.println();
+//                }
+        
+//        * * * * * * 
+//            * * * * * 
+//                * * * * 
+//                    * * * 
+//                        * * 
+//                            *
+
+//            for (int i = 0; i <= n ; i++) {
+//
+//                for (int j = 0; j < i -1 + 1  ; j++) {
+//                    System.out.print("    ");
+//                }
+//
+//                for (int j = 0; j <= n-i ; j++) {
+//                    System.out.print("* ");
+//                }
+//
+//                System.out.println();
+//            }
+
+
+//          *
+//       *  *
+//     *  *  *
+//   *  *  *  *
+// *  *  *  *  *
+
+//        for (int i = 0; i <= n ; i++) {
+//            for (int j = 0; j <= n-i -1  ; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 0; j < i ; j++) {
+//                System.out.print(" * ");
+//            }
+//            for (int j = 0; j <= n-i -1  ; j++) {
+//                System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 0; i <=n  ; i++) {
+            for (int j = 0; j <= n- i - 1 ; j++) {
                 System.out.print("  ");
             }
-            for (int j = 0; j < i+1; j++) {
+            for (int j = 0; j < i  ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
+
+            if (i >= n ){
+                for (int j = 0; j < ; j++) {
+                    
+                }
+            }
         }
     }
 }
+
