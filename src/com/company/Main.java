16111250662f8363808d80fe7a23bbb2fc3d4b46
VaishnavChaudhari <65059084+VaishnavChaudhari@users.noreply.github.com>
2020-10-06 21:35:32 +0530

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,9 +15,9 @@ public class Main {
 //                *
 
         Scanner cs = new Scanner(System.in);
-                int n = cs.nextInt();
+        int n = cs.nextInt();
 
-                
+
 //                for (int i = 0; i <= n  ; i++) {
 //                    for (int j = 0; j < i - 1 + 1 ; j++) {
 //                        System.out.print("  ");
@@ -27,9 +27,8 @@ public class Main {
 //                    }
 //                    System.out.println();
 //                }
-        
-        
-        
+
+
 //        * * * * * * 
 //            * * * * * 
 //                * * * * 
@@ -50,7 +49,6 @@ public class Main {
 //                System.out.println();
 //            }
 
-        
 
 //         *
 //       *  *
@@ -71,8 +69,6 @@ public class Main {
 //            System.out.println();
 //        }
 
-        
-        
 
 //                *
 //              * *
@@ -107,8 +103,7 @@ public class Main {
 ////                }
 ////            }
 ////        }
-        
-        
+
 
 //        * * *   * * *
 //        * *       * *
@@ -158,8 +153,8 @@ public class Main {
 //            }
 //            System.out.println();
 //        }
-        
-        
+
+
 //
 //                                            1
 //                                        1  2  3
@@ -180,8 +175,7 @@ public class Main {
 //            }
 //            System.out.println();
 //        }
-        
-        
+
 
 //                                     1
 //                                   2 0 2
@@ -211,7 +205,106 @@ public class Main {
 //            System.out.println();
 //       }
 
+//                                                       0
+//                                                     9 0 9
+//                                                   8 9 0 9 8
+//                                                 7 8 9 0 9 8 7
+//                                               6 7 8 9 0 9 8 7 6
+//                                             5 6 7 8 9 0 9 8 7 6 5
+//                                           4 5 6 7 8 9 0 9 8 7 6 5 4
+//                                        3 4 5 6 7 8 9 0 9 8 7 6 5 4 3
+//                                      2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2
+//                                    1 2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2 1
+
+
+//        System.out.println("                    "+0);
+//        for (int i = 0; i < n ; i++) {
+//            for (int j = 0; j <= n- i -1 ; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = n -i ; j >= n -i ; j--) {
+//                System.out.print(j+" ");
+//            }
+//
+//            for (int j = n -i +1; j <= n ; j++) {
+//                System.out.print(j+" ");
+//            }
+//
+//
+//
+//            for (int j = 0; j <= 0 ;j++) {
+//                System.out.print(0+" ");
+//            }
+//
+//            for (int j = n ; j >= n -i ; j--) {
+//                System.out.print(j+" ");
+//            }
+////            for (int j = n -i +1; j <= n ; j++) {
+////                System.out.print(j+" ");
+////            }
+//
+////            for (int j = i; j <= i && j != 1; j++) {
+////
+////                System.out.print(j+" ");
+////            }
+//            System.out.println();
+//       }
+
+//        * 
+//        * * 
+//        *   * 
+//        *     * 
+//        * * * * *
+
+//        System.out.println("* ");
+//        for (int i = 0; i < n ; i++) {
+//            System.out.print("* ");
+//            for (int j = 0; j < i ; j++) {
+//                System.out.print("  ");
+//            }
+//            System.out.print("* ");
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i <= n + 1 ; i++) {
+//            System.out.print("* ");
+//        }
+
+//            *
+//          * *
+//        *   *
+//      *     *
+//    * * * * *
+
+//        for (int i = 0; i < n ; i++) {
+//            for (int j = 0; j <= n -i ; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = i; j <=  i  ; j++) {
+//                System.out.print("* ");
+//            }
+//
+//            if (i > 1 && i < n -1){
+//                for (int j = 2; j < (2*i) ;j++) {
+//                    System.out.print(" ");
+//
+//                }
+//
+//            }
+//
+//            if (i > 0 && i < n-1){
+//                for (int j = 0; j <=0 ; j++) {
+//                    System.out.print("* ");
+//                }
+//            }
+//            if (i == n - 1 ){
+//                for (int j = 0; j < n-1 ; j++) {
+//                    System.out.print("* ");
+//            }
+//            }
+//            System.out.println();
+//        }
+        
 
     }
 }
-
